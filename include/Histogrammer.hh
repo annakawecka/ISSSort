75685a2d5dc5af05db1bef37365e38ecdd1f6f57
Anna Kawecka <anna.kawecka@chalmers.se>
2026-02-20 13:02:39 +0100

    Back to full EBIS cut range

--- include/Histogrammer.hh
+++ include/Histogrammer.hh
@@ -227,7 +227,7 @@ public:
 		else return false;
 	};
 	inline bool	OnBeam( std::shared_ptr<ISSArrayEvt> a ){
-		if( a->GetTime() - read_evts->GetEBIS() >= 150 &&
+		if( a->GetTime() - read_evts->GetEBIS() >= 0 &&
 			a->GetTime() - read_evts->GetEBIS() < react->GetEBISOnTime() ) return true;
 		else return false;
 	};
@@ -242,7 +242,7 @@ public:
 		else return false;
 	};
 	inline bool	OnBeam( std::shared_ptr<ISSCDEvt> f ){
-		if( f->GetTime() - read_evts->GetEBIS() >= 150 &&
+		if( f->GetTime() - read_evts->GetEBIS() >= 0 &&
 		    f->GetTime() - read_evts->GetEBIS() < react->GetEBISOnTime() ) return true;
 		else return false;
 	};
